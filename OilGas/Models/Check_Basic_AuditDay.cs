31280428d6a3d5933c87527f581630d96674108e
brianlin12345 <brianlin12345@gamil.com>
2024-01-03 11:03:30 +0800

    a

--- OilGas/Models/Check_Basic_AuditDay.cs
+++ OilGas/Models/Check_Basic_AuditDay.cs
@@ -153,5 +153,464 @@ namespace OilGas.Models
         [ColumnDef(Visible = false)]
         public string A06_Note { get; set; }
 
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string B01 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? B01_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string B01_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string B01_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string B02 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? B02_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string B02_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string B02_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string C01 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? C01_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string C01_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string C01_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string C02 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? C02_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string C02_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string C02_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string C03 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? C03_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string C03_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string C03_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string D01 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? D01_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string D01_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string D01_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string D02 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? D02_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string D02_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string D02_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string D03 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? D03_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string D03_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string D03_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string D04 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? D04_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string D04_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string D04_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string E01 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? E01_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string E01_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string E01_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string E02 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? E02_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string E02_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string E02_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string E03 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? E03_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string E03_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string E03_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string F01 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? F01_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string F01_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string F01_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string F02 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? F02_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string F02_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string F02_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string F03 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? F03_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string F03_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string F03_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string G01 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? G01_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string G01_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string G01_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string H01 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? H01_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string H01_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string H01_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string H02 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? H02_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string H02_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string H02_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string H03 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? H03_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string H03_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string H03_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string H04 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? H04_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string H04_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string H04_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string I01 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? I01_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string I01_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string I01_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string I02 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? I02_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string I02_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string I02_Note { get; set; }
+
+        [StringLength(1)]
+        [Display(Name = "檢查結果")]
+        [ColumnDef(Visible = false, EditType = EditType.Radio, SelectItems = "{\"N\":\"不符合\",\"Y\":\"符合\"}")]
+        public string j01 { get; set; }
+
+        [Display(Name = "檢查方法")]
+        [ColumnDef(Visible = false,
+            EditType = EditType.Select, SelectItemsClassNamespace = CheckWaySelectItems.AssemblyQualifiedName)]
+        public int? j01_Way { get; set; }
+
+        [StringLength(300)]
+        [Display(Name = "改善情形")]
+        [ColumnDef(Visible = false)]
+        public string j01_Improve { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "備註")]
+        [ColumnDef(Visible = false)]
+        public string j01_Note { get; set; }
     }
 }
