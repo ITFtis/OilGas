20ccd0b6ece98aaa2707a79c8ae0c8c3f4193435
brianlin12345 <brianlin12345@gamil.com>
2024-01-12 10:28:13 +0800

    汽車加油站 批次變更負責人 v1 menu

--- OilGas/Controllers/CarFuel/CarFuel_TGOS_AreaController.cs
+++ OilGas/Controllers/CarFuel/CarFuel_TGOS_AreaController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace OilGas.Controllers.CarFuel
 {
-    [Dou.Misc.Attr.MenuDef(Id = "CarFuel_TGOS_Area", Name = "地圖環域查詢", MenuPath = "加油站/A管理專區", Action = "Index", Index = 3, Func = Dou.Misc.Attr.FuncEnum.ALL, AllowAnonymous = false)]
+    [Dou.Misc.Attr.MenuDef(Id = "CarFuel_TGOS_Area", Name = "地圖環域查詢", MenuPath = "加油站/A管理專區", Action = "Index", Index = 4, Func = Dou.Misc.Attr.FuncEnum.ALL, AllowAnonymous = false)]
     public class CarFuel_TGOS_AreaController : Controller
     {
         // GET: CarFuel_TGOS_Area
---
+++ OilGas/Controllers/CarFuel/CarFuel_UpdateController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OilGas.Controllers.CarFuel
+{
+    [Dou.Misc.Attr.MenuDef(Id = "CarFuel_Update", Name = "負責人批次變更", MenuPath = "加油站/A管理專區", Action = "Index", Index = 2, Func = Dou.Misc.Attr.FuncEnum.ALL, AllowAnonymous = false)]
+    public class CarFuel_UpdateController : Controller
+    {
+        // GET: CarFuel_Update
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
